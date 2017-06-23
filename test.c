6779b8a9eb64922b4036670486662af1a1824191
Martin T. H. Sandsmark <martin.sandsmark@kde.org>
2017-06-23 11:42:35 +0200

    reorganize dir

--- test.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-	printf("This is a test code for uploading and downloading\n");
-
-	return 0;
-}
--- test_dir/test.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-  printf("This is the second test file\n");
-
-  return 0;
-}
